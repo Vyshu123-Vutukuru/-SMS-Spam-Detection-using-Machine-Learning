f921236bc0380e400a16df6cd7707f81046c0c1a
Vyshu123-Vutukuru <vtu21932@veltech.edu.in>
2025-07-27 11:23:04 +0530

    Vectorization.py

--- Vectorization.py
+++ Vectorization.py
@@ -1,1 +1,6 @@
-vectorizer = TfidfVectorizer()X = vectorizer.fit_transform(df['cleaned'])y = df['label']X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df['cleaned'])
+y = df['label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
